9e8d6eb37910aa53ff1d73025d8526fa525c0c1b
Thang <thangnus@gmail.com>
2021-05-10 23:47:51 +1200

    fix(deploy-script): add aws region

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 
 BUCKET_NAME=boardgamehub-vue-webapp-s3-bucket
 DIST_FOLDER=dist
+REGION=ap-southeast-2
 
 echo "Deploying to bucket $BUCKET_NAME"
 
-aws s3 sync --delete $DIST_FOLDER "s3://$BUCKET_NAME"
+aws s3 sync --delete $DIST_FOLDER "s3://$BUCKET_NAME" --region $REGION
 
 echo "Deployed Finished!"
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>Testing Github Actions Take 2</h1>
+    <h1>Testing Github Actions Take 3</h1>
   </div>
 </template>
